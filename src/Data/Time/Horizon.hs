23da60c9154fcd922ad59558e46ef1d779a62054
Joel Stanley <intractable@gmail.com>
2015-02-04 11:06:35 -0600

    Initial commit

---
+++ src/Data/Time/Horizon.hs
@@ -0,0 +1,16 @@
+{-
+  Module     : Data.Time.Horizon
+  License    : BSD3
+  Copyright  : (C) 2015 Joel Stanley
+  Maintainer : Joel Stanley <intractable@gmail.com>
+  Stability  : provisional
+  
+  Provides sunrise and sunset time calculations from latitude and longitude
+  coordinates.
+
+  See https://en.wikipedia.org/wiki/Sunrise_equation.
+
+-}
+
+module Data.Time.Horizon where
+
